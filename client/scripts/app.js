773f145d69264206d99291f8da5ae9b2c23f5328
wettowelreactor <camille@rousselfamily.com>
2015-02-16 15:35:24 -0800

    added basic styling and messaging

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,9 @@ var getMessages = function() {
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data: {
+      limit: 1000
+    },
     success: success,
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -15,7 +18,17 @@ var getMessages = function() {
 };
 
 var success = function(data) {
-
+  var $messages = $('.messages');
+  for(var i = 0; i < data.results.length; i++) {
+    $messages.append('<div class=message></div>');
+    var $message = $('.message:last-child');
+    var text = encodeURI(data.results[i].text);
+    text = text.replace(/\%20/g, ' ');
+    var user = encodeURI(data.results[i].username);
+    user = user.replace(/\%20/g, ' ');
+    $message.append('<div class="user">'+user+': </div>');
+    $message.append('<div class="text">'+text+'</div>');
+  }
   console.log(data);
 };
 
