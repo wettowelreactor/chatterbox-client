e10259d2a6d66d046137b6980d8e4971a8498a84
wettowelreactor <camille@rousselfamily.com>
2015-02-16 14:53:01 -0800

    basic message response working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,23 @@
 // YOUR CODE HERE:
 
+var getMessages = function() {
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: success,
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+var success = function(data) {
+
+  console.log(data);
+};
+
+
+getMessages();
